ac8a323aced613ab8fcacc0d3b5ae3371365647f
Josh Shevach <josh@composer.trade>
2025-09-09 12:57:50 -0500

    fix(web): remove the need to reference the cloud project

--- codegen.ts
+++ codegen.ts
@@ -1,39 +1,16 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
-const hasuraToken = 'need the ddn token to run codegen';
-const hasuraEndpoint = 'https://fitting-piglet-7078-9b7c143d62.ddn.hasura.app/graphql';
+const hasuraEndpoint = 'http://localhost:3280/graphql';
 
 const config: CodegenConfig = {
-  schema: [
-    {
-      [hasuraEndpoint]: {
-        headers: {
-          'x-hasura-ddn-token': hasuraToken,
-        },
-      },
-    },
-  ],
+  schema: hasuraEndpoint,
   documents: ['src/**/*.ts', 'src/**/*.tsx'],
   ignoreNoDocuments: true,
   generates: {
-    './src/lib/gql/gen/': {
-      preset: 'client',
-      config: {
-        documentMode: 'string',
-      },
-    },
     './schema.graphql': {
       plugins: ['schema-ast'],
       config: {
-        schema: [
-          {
-            [hasuraEndpoint]: {
-              headers: {
-                'x-hasura-ddn-token': hasuraToken,
-              },
-            },
-          },
-        ],
+        schema: hasuraEndpoint,
         includeDirectives: true,
       },
     },
